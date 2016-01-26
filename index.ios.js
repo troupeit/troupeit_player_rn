7debde562e03eceec879a0e45576c6df1ab79c25
John Adams <jna@twitter.com>
2016-01-25 18:06:06 -0800

    it compies but many js errors

--- index.ios.js
+++ index.ios.js
@@ -3,50 +3,153 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
+
+/* ES5 way */
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
+  Navigator,
   Text,
   View
 } from 'react-native';
 
-class troupeit_player_rn extends Component {
-  render() {
+/* this is the old way of doing includes */ 
+
+var Home = require('./js/components/home');
+var NavigationBar = require('react-native-navbar');
+var ShowList = require('./js/components/show-list');
+var styles = require('./js/utils/styles');
+var Video = require('react-native-video');
+var App = require('./js/app.js')
+var PlayerActions = require('./js/actions/player-actions')
+var PlayerStore = require('./js/stores/player-store')
+var ListenerMixin = require('alt-mixins/ListenerMixin')
+var SideMenu = require('react-native-side-menu');
+var Menu = require('./js/components/menu')
+var CurrentTrack = require('./js/components/current-track')
+
+var navigation = React.createClass ({
+
+  renderScene(route, navigator) {
+    var Component = route.component;
+    var navBar = route.navigationBar;
+
+    if (navBar) {
+      navBar = React.addons.cloneWithProps(navBar, {
+        navigator: navigator,
+        route: route
+      });
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={styles.navContainer}>
+
+        {navBar}
+        <Component navigator={navigator} {...route.props} />
+        <CurrentTrack />
       </View>
     );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  render: function() {
+    return (
+      <Navigator
+        ref={(navigator) => {
+          this._navigator = navigator;
+        }}
+        renderScene={this.renderScene}
+        style={styles.navContainer}
+        configureScene={(route) => ({
+          ...route.sceneConfig || Navigator.SceneConfigs.FloatFromRight
+        })}
+        initialRoute={{
+          component: TIPlayer,
+          navigationBar: <NavigationBar title="Archive.org Audio" hidePrev={true} prevTitle='haha' />,
+          title: 'Archive.org Audio',
+        }}/>
+    );
   },
 });
 
-AppRegistry.registerComponent('troupeit_player_rn', () => troupeit_player_rn);
+/* This is the main player page, triggered by navigation */
+var TIPlayer = React.createClass({
+  mixins: [ListenerMixin],
+
+  getInitialState: function() {
+    return {
+      currentTrack: {
+        url: ''
+      },
+      playing: false
+    }
+  },
+
+  componentDidMount: function() {
+    this.listenTo(PlayerStore, this._onChange)
+  },
+
+  _onChange: function() {
+    this.setState({
+      currentTrack: {
+        url: PlayerStore.getState().url
+      },
+      playing: PlayerStore.getState().playing
+    })
+  },
+
+  _playNextTrack: function() {
+
+    var state = PlayerStore.getState(),
+        index,
+        url;
+
+    if(state.index === state.tracks.length -1 ) {
+      index = 0;
+      url = 'https://archive.org' + state.dir + '/' + state.tracks[0].name
+    } else {
+      index = Number(state.index) + 1;
+      url = 'https://archive.org' + state.dir + '/' + state.tracks[index].name
+    }
+
+    PlayerActions.update({
+      url: url,
+      index: index,
+      tracks: state.tracks,
+      dir: state.dir
+    })
+  },
+
+  _onEnd: function() {
+    this._playNextTrack();
+  },
+
+  render: function() {
+
+    var menu = <Menu navigator={this.props.navigator}/>;
+
+    return (
+      <SideMenu menu={menu}>
+        <View>
+          <Home navigator={this.props.navigator} />
+
+          { this.state.currentTrack.url !== '' ?
+          <Video source={{uri: this.state.currentTrack.url}}
+                 rate={1.0}
+                 volume={1.0}
+                 muted={false}
+                 paused={!this.state.playing}
+                 resizeMode="contain"
+                 repeat={false}
+                 style={styles.backgroundVideo}
+                 onLoad={this._onLoad}
+                 onEnd={this._onEnd} /> : null }
+        </View>
+      </SideMenu>
+    );
+  }
+});
+
+
+AppRegistry.registerComponent('troupeit_player_rn', () => navigation);
