c54898ba4d2e27827048f09be2c096d811dac2cf
John Adams <jna@twitter.com>
2016-03-11 13:45:04 -0800

    partially broken

---
+++ js/stores/show-store.js
@@ -0,0 +1,44 @@
+var alt = require('../alt');
+import ApiUtils from "../utils/ApiUtils"
+var config = require('../utils/config.js');
+var ShowActions = require('../actions/show-actions')
+
+/* 
+ * take a look at ( for dealing with ajax in stores ) tomorrow... 
+ * https://stackoverflow.com/questions/26632415/where-should-ajax-request-be-made-in-flux-app 
+ */
+class ShowStore {
+  constructor() {
+
+   this.show = [];
+
+   this.bindListeners({
+       handleFetchShow: ShowActions.FETCH_SHOW,
+       handleFetchShowComplete: ShowActions.FETCH_SHOW_COMPLETE,
+       handleFetchShowError: ShowActions.FETCH_SHOW_ERROR,
+   });
+
+  }
+
+  handleFetchShow() {
+    console.log("hfs");
+    this.show = [];
+  }
+ 
+  handleFetchShowComplete(show) {
+    console.log("hfsc");
+    this.show = show;
+  }
+
+  handleFetchShowError(error) {
+    console.log("hfsse");
+    this.error = error;
+  }
+
+  getState() {
+    return this.getState()
+  }
+
+}
+
+module.exports = alt.createStore(ShowStore, 'ShowStore');
