c138df91e5ef0cc01e8a236ba71b89d39f933160
John Adams <jna@twitter.com>
2016-03-17 14:47:27 -0700

    add config bis

--- js/components/ConfigButton.js
+++ js/components/ConfigButton.js
@@ -7,17 +7,25 @@ var {
 } = React;
 
 var Button = require('react-native-button');
+var styles = require('../utils/styles.js');
 
 /* the button that opens the config drawer */
+var {Icon, } = require('react-native-icons');
+
 var ConfigButton = function() {
 	    return (
 		    <View>
 		    <Button style={{color: 'green'}}>
-		    +
+          <Icon
+             name='fontawesome|cog'
+             size={20}
+             width={20}
+             height={20}     
+             color='white'
+             style={styles.configButton}
+             />
 		    </Button>
 		    </View>
-
-
 	    )
 };
 
